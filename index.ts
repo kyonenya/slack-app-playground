14412b353698799398da35dccba534ad689420e3
kyonenya <kyon.musica@gmail.com>
2023-09-19 05:11:46 +0900

    feat: Hey there @kyonenya !

--- index.ts
+++ index.ts
@@ -1,5 +1,12 @@
 import { app } from "./app";
 
+// "hello" を含むメッセージをリッスンします
+app.message("hello", async ({ message, say }) => {
+  // イベントがトリガーされたチャンネルに say() でメッセージを送信します
+  // 型エラーを解消するために any を使っています。
+  await say(`Hey there <@${(message as any).user}>!`);
+});
+
 (async () => {
   // アプリを起動
   await app.start(process.env.PORT || 3000);
